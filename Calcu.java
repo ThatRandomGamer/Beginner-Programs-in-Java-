53af470899b0e47a19cbaa231109353fd85de976
ThatRandomGamer <39994165+ThatRandomGamer@users.noreply.github.com>
2020-01-26 17:55:56 +0530

    Added my first two beginner java projects for you
    This is a console-based calculator and a console-based random fortune cookie program. The code is fully commented and I try to comment on each and every line for these beginner projects. I hope to record my journey and also make some good teaching exercises for java beginners. If you are trying to find some exercises then first Read what the program does, try to do it on your own, and if you need help check out how I did it.

---
+++ Calcu.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class Calcu {
+
+//Ran well on first try, had to search up how to use scanner tho
+	
+//First we declare the variables
+	
+	public static int firstNum;
+	public static int secondNum;
+	public static String opperand;
+	static int sum = 0;
+	
+//Main method, classic	
+	
+	public static void main(String[] args) {
+		
+//Setup scanner object
+		
+		Scanner sc = new Scanner(System.in);
+		//Instructions for the user, could have been better, I am just lazy
+		
+		System.out.println("Enter First Num, Second Num and + - * /");
+		//Now we get values for the variables from the user
+		
+		firstNum = sc.nextInt();
+		secondNum = sc.nextInt();
+		
+		//Next() method defaults a string, have to specify other data types for other data types i.e int
+		opperand = sc.next();
+//Setup switch cases
+	switch(opperand) {
+		case "+":
+			sum = firstNum + secondNum;
+			System.out.println( "Sum is" + " " + sum); //I kind of forgot how to print a variable with a "" but later on i remembered, lol
+			break;
+		case "-":	
+			sum = firstNum - secondNum;
+			System.out.println( "Sum is" + " " + sum);
+			break;
+		case "*":	
+			sum = firstNum * secondNum;
+			System.out.println( "Sum is" + " " + sum);
+			break;
+		case "/":	
+			sum = firstNum / secondNum;
+			System.out.println( "Sum is" + " " + sum);
+			break;
+		
+		
+		}
+		
+	}
+
+}
---
+++ FortuneCookie.java
@@ -0,0 +1,24 @@
+// By the way, all of the projects in this workspace will have comments. This is the learning workspace after all! Remeber, always comment
+//Imports java Random
+import java.util.Random;
+//Creates a public class named FortuneCookie
+public class FortuneCookie {
+//Ah yes, the main method. Never forget!
+	
+      public static void main(String args []) {
+    	  
+//Now this is the juicy part! This creates a string array called fortunes and it is filled with, well, fortunes!
+        
+    	  String[] fortunes  = {"The fortune you seek is in another cookie." , "Test!","A conclusion is simply the place where you got tired of thinking" , "A closed mouth gathers no feet" ,"A cynic is only a frustrated optimistt" , "You will die alone and poorly dressed" , "If you look back, you'll soon be going that way" ,"You will live long enough to open many fortune cookies" , "You found an easter egg! IT is useless though, but there are a few more, keep looking!" , "Some fortune cookies contain no fortune.", "Don't let statistics do a number on you" ,"You will receive a fortune cookie." , "I am worth a fortune." , "You love Chinese food." ,"All fortunes are wrong except this one." , "It's about time I got out of that cookie" , "Minecraft is the best" ,"The road to riches is paved with homework." , "Actions speak louder than fortune cookies" , "Made by Rudra Singh!" ,"Flattery will go far tonight" , "Help! I am being held prisoner in a fortune cookie factory" , "I am, Iron Man!" ,"Fortune not found? Abort, Retry, Ignore." , "Don't forget you are always on our minds." , "You can always find happiness at work on Friday."};
+//Creates a Random objects named fortunecookie aka fc
+         
+         Random fc = new Random();
+//Creates an integer or int variable called fortunecookie key or fcKey.We tell it to create a random integer and fortunecookie.lenght is the range.
+       
+         int fcKey = fc.nextInt(fortunes.length); //The length is 26 currently. I could add more fortunes to it later but I think this is a pretty good number
+//It prints or tells the screen to display the randomly genrated fortune
+         
+         System.out.println("You open your cookie. It reads: " +fortunes[fcKey]);
+            	
+            }
+}
